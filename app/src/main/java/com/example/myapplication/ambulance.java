94e3df5d01fdb35d8e25cee76da3e4be6865e864
krishna <krishnakshettri@gmail.com>
2021-05-21 23:38:28 +0530

    ambulance

--- app/src/main/java/com/example/myapplication/ambulance.java
+++ app/src/main/java/com/example/myapplication/ambulance.java
@@ -3,17 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ambulance extends AppCompatActivity {
+
     private TextInputLayout textInputLayout;
+    private AutoCompleteTextView dropDownText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance);
 
-        textInputLayout = findViewById(R.layout.select_gender);
+        dropDownText = findViewById(R.id.autoComplete_gender);
+        textInputLayout = findViewById(R.id.select_gender);
+        String[] gender= new String[]{
+                "Please Select",
+                "Male",
+                "Female",
+                "None"
+        };
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(ambulance.this,
+                R.layout.gender_items,
+                gender
+        );
+        dropDownText.setAdapter(adapter);
     }
 }
